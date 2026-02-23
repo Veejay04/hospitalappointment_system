87c06783a98b543146110438f8170298d5359299
Veejay04 <faelnarv@gmail.com>
2026-02-23 13:24:19 +0800

    Used POSTMAN/THUNDERBIRD for CRUD

--- hospitalappointment_system/settings.py
+++ hospitalappointment_system/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hospitalappointment_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospital_db',   
+        'USER': 'root',          
+        'PASSWORD': '',         
+        'HOST': 'localhost',     
+        'PORT': '3306',          
     }
 }
 
